aba15b4d68d09d7b9222f340a976c14eed3a2534
Sulai man <gamingsulaiman333@gmail.com>
2026-09-08 14:31:29 +0530

    Update player.py

--- app/youtube/player.py
+++ app/youtube/player.py
@@ -56,6 +56,25 @@ for pattern in patterns:
     text
   )
 
+if match:
+
+  query = match.group(1)
+  break
+
+query = query.strip()
+
+video_id =get_vid(query)
+
+if not video_id:
+  return None
+
+return (
+  "htpps://www.youtube.com/embed/"
+  +video_id
+  +"?autoplay=1&mute=0"
+)
+
+
 
   
 
