6916e7e9afda3456a1a65e0007f14e429974a20c
Sulai man <gamingsulaiman333@gmail.com>
2026-09-10 15:47:23 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,92 @@
-from flask import Flask, render_template
-from app.youtube import youtube_bp
+import os
 
+from flask import Flask,request,jsonify,render_template
+from flask_cors import CORS
+
+from app.gmail import(
+is_email_command,
+extract_email,
+create_gmail_url,
+generate_email_with_gemini
+)
+
+from app:youtube import youtube_bp
 
 def create_app():
 
-    app = Flask(__name__)
+    app=Flask(__name__)
+    CORS(app)
 
-    app.register_blueprint(
-        youtube_bp,
-        url_prefix="/youtube"
+#youtube
+app.register_blueprint(
+    youtube_bp,
+    url_prefix="youtube"
+)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+#html
+@app.route("/html")
+def html():
+    return render_templates("index.html")
+
+#health
+@app.route("/health")
+def health():
+    return jsonify({
+        "status":"ok",
+        "service":"Nova AI Agent"
+    })
+
+#gmail AI Agent
+@app.route("/agent",methods=["POST"])
+def agent():
+
+    try:
+        data=request.get_json(silent=True) or {}
+        command = dta.get("command","").strip()
+
+if not command:
+    return jsonify({
+        "success":False,
+        "message":"command is required"
+    }),400
+
+if not is_email_command(command):
+    return jsonify({
+        "success":False,
+        "message":"please give a Gmail command."
+    }),400
+
+recipient = extract_email(command)
+
+email = geneate_email_with_gemini(command)
+
+return jsonify({
+    "sucess": True,
+    "Type": "email",
+    "email_generated":True,
+    "recipitent": recipient,
+    "subject":email["subject"],
+    "body":email["body"],
+    "gmail_url": create_gmail_url(
+        email["subject"],
+        email["body"],
+        recipient
     )
+})
+except exception as e:
 
-    @app.route("/")
-    def home():
-        return render_template("index.html")
+return jsonify({
+    "sucess":False,
+    "message":str(e)
+}),500
+return app
 
-    @app.route("/html")
-    def html():
-        return render_template("index.html")
 
-    return app
-     
+
+
+
     
